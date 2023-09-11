c9b1be58ae05f65a5c3f575e5542b35cdc01c072
bwys14 <142304139+bwys14@users.noreply.github.com>
2023-09-11 23:35:08 +0200

    Update dummy.py

--- tests/dummy.py
+++ tests/dummy.py
@@ -1,2 +1,12 @@
-import pytest
-assert True
+from pyspark.sql import SparkSession
+from pytest import fixture
+
+
+@fixture(name="spark_sess", scope="session")
+def spark_session_fixture() -> SparkSession:
+    """
+    This function initializes spark session
+    """
+    spark_sess = SparkSession.builder.appName("test_session").getOrCreate()
+    yield spark_sess
+    spark_sess.stop()
