4aa2d56d56d1d640df060a68e3f1cecec93cf588
bwys14 <142304139+bwys14@users.noreply.github.com>
2023-09-12 08:20:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-rqs_file = open("requirements.txt", "r")
-rq = rqs_file.read().split("\n")
-rqs_file.close()
-
 setup(
    name='KP-DataProcessing',
    version='1.0.0',
@@ -11,6 +7,6 @@ setup(
    author_email='beata.wysocka@capgemini.com',
    packages=find_packages(exclude=['tests']),
    description='Package for dedicated data processing within company dealing with Bitcoin trading',
-   long_description=open('README.txt').read(),
-   install_requires = rqs,
+   long_description=open('README.MD', "r").read(),
+   install_requires = open("requirements.txt", "r").read().split("\n"),
 )
